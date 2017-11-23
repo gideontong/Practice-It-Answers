72fb3f77b86a7538f74417355a0550a0d0c327f1
Gideon Tong <gideontong@hotmail.com>
2017-11-22 22:01:33 -0800

    More programs

---
+++ Building-Java-Programs-4th-Ed/Ch01-Intro-to-Java-Programming/Stewie.java
@@ -0,0 +1,9 @@
+/*
+ * Solution by Gideon Tong
+ */
+
+ public class Stewie {
+     public static void main(String[] args) {
+         System.out.println("//////////////////////\n|| Victory is mine! ||\n\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+     }
+}
---
+++ Building-Java-Programs-4th-Ed/Ch01-Intro-to-Java-Programming/WellFormed.java
@@ -0,0 +1,12 @@
+/*
+ * Solution by Gideon Tong
+ */
+
+public class WellFormed {
+    public static void main(String[] args) {
+        System.out.println("A well-formed Java program has\na main method with { and }\nbraces.\n");
+        System.out.println("A System.out.println statement\nhas ( and ) and usually a");
+        System.out.println("String that starts and ends\nwith a \" character.");
+        System.out.println("(But we type \\\" instead!)");
+    }
+}
