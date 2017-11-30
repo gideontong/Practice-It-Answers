e0d5a4d8190b3f3efe568615f5b41d3bf0771dae
Gideon Tong <gideontong@hotmail.com>
2017-11-29 23:28:38 -0800

    4th ed ch. 1 and ch. 2 programs

---
+++ Building-Java-Programs-3rd-Ed/Ch02-Primitive-Data/displacement.java
@@ -0,0 +1,5 @@
+// Solution by Gideon Tong
+
+double s = 12, v = 3.5, a = 9.8, t = 10;
+double x = s+v*t+0.5*a*Math.pow(t,2);
+System.out.print(x);
---
+++ Building-Java-Programs-3rd-Ed/Ch02-Primitive-Data/loopSquares.java
@@ -0,0 +1,5 @@
+// Solution by Gideon Tong
+
+for(int i=1;i<=10;i++) {
+    System.out.print((int)Math.pow(i, 2) + " ");
+}
